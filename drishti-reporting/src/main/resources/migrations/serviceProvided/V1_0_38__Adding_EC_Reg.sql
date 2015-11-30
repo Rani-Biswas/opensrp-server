8e96aa44d3bd3fca2da3de910e9361cd479f0b09
rakesh.b <rakesh.b@dhanushinfotech.net>
2015-11-30 15:53:28 +0530

    Formating sql file

--- drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_38__Adding_EC_Reg.sql
+++ drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_38__Adding_EC_Reg.sql
@@ -1,38 +1,17 @@
---
--- PostgreSQL database dump
---
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
-SET search_path = report, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: ec_reg; Type: TABLE; Schema: report; Owner: dhanush; Tablespace: 
---
-
-CREATE TABLE ec_reg (
+CREATE TABLE report.ec_reg (
     id integer NOT NULL,
     entityid character varying(200) NOT NULL,
     phonenumber character varying(200) NOT NULL
 );
 
 
-ALTER TABLE ec_reg OWNER TO dhanush;
+ALTER TABLE report.ec_reg OWNER TO dhanush;
 
 --
 -- Name: ec_reg_id_seq; Type: SEQUENCE; Schema: report; Owner: dhanush
 --
 
-CREATE SEQUENCE ec_reg_id_seq
+CREATE SEQUENCE report.ec_reg_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -40,20 +19,20 @@ CREATE SEQUENCE ec_reg_id_seq
     CACHE 1;
 
 
-ALTER TABLE ec_reg_id_seq OWNER TO dhanush;
+ALTER TABLE report.ec_reg_id_seq OWNER TO dhanush;
 
 --
 -- Name: ec_reg_id_seq; Type: SEQUENCE OWNED BY; Schema: report; Owner: dhanush
 --
 
-ALTER SEQUENCE ec_reg_id_seq OWNED BY ec_reg.id;
+ALTER SEQUENCE report.ec_reg_id_seq OWNED BY report.ec_reg.id;
 
 
 --
 -- Name: id; Type: DEFAULT; Schema: report; Owner: dhanush
 --
 
-ALTER TABLE ONLY ec_reg ALTER COLUMN id SET DEFAULT nextval('ec_reg_id_seq'::regclass);
+ALTER TABLE report.ONLY ec_reg ALTER COLUMN id SET DEFAULT nextval('report.ec_reg_id_seq'::regclass);
 
 
 --
